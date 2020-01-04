d8d3d2d8af0014d83ea62a78de14921e01b40453
Your Name <you@example.com>
2020-01-04 23:14:26 +0100

    Updates

---
+++ precache-manifest.fab7d0e7d34890cd3659fb8861e65235.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279ad9cb79b34617529fa35712e0a9f2",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "704fc89b3c5a24a0dae0",
+    "url": "/myapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5bd02e3881566196a9d4",
+    "url": "/myapp/static/css/main.e685b6bf.chunk.css"
+  },
+  {
+    "revision": "704fc89b3c5a24a0dae0",
+    "url": "/myapp/static/js/2.6ac2ca54.chunk.js"
+  },
+  {
+    "revision": "a5a8b7af208d27ffe80a0e6f18f394d1",
+    "url": "/myapp/static/js/2.6ac2ca54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bd02e3881566196a9d4",
+    "url": "/myapp/static/js/main.dae6d5b1.chunk.js"
+  },
+  {
+    "revision": "fd3f1e81b4b57dc6e674",
+    "url": "/myapp/static/js/runtime-main.e087ead8.js"
+  },
+  {
+    "revision": "d3007d2809b8be7a7747ec7507e7965e",
+    "url": "/myapp/static/media/logo.d3007d28.png"
+  },
+  {
+    "revision": "06ea83c32a7c2f5ba80770a4645acb43",
+    "url": "/myapp/static/media/special.06ea83c3.jpg"
+  }
+]);
